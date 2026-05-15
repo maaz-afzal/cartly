3c228082215c1f69b0b9c4416245790cd75b44cb
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-15 19:12:08 +0500

    add category filter functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,32 @@
+import { Loader } from "lucide-react";
 import Button from "../components/ui/Button";
 import ProductCard from "../components/ui/ProductCard";
 import useFetch from "../hooks/useFetch";
+import { useState } from "react";
 
 const Home = () => {
   const { data, loading, error } = useFetch(
     "https://fakestoreapi.com/products",
   );
 
+  const [filter, setFilter] = useState("");
+
   if (loading)
-    return <div className="w-full max-w-6xl mx-auto mt-8 px-4">Loading...</div>;
+    return (
+      <div className="w-full max-w-6xl mx-auto">
+        <Loader
+          size={50}
+          className="mx-auto flex items-center justify-center h-screen animate-spin"
+        />
+      </div>
+    );
 
   if (error)
     return <div className="w-full max-w-6xl mx-auto mt-8 px-4">{error}</div>;
+
+  const handleFilter = (category) => {
+    setFilter(category);
+  };
 
   return (
     <div className="w-full max-w-6xl mx-auto mt-8 px-4">
@@ -37,28 +52,49 @@ const Home = () => {
       <section className="mt-8 flex flex-wrap justify-between items-center">
         <h2 className="text-2xl font-serif font-bold">Featured Products</h2>
         <div className="flex gap-2">
-          <Button label="All" isActive={true} />
-          <Button label="Electronics" isActive={false} />
-          <Button label="Jewelery" isActive={false} />
-          <Button label=" Men Clothing" isActive={false} />
-          <Button label="Women Clothing" isActive={false} />
+          <Button
+            label="All"
+            isActive={filter === "All" || filter === "" ? true : false}
+            onClick={() => handleFilter("All")}
+          />
+          <Button
+            label="Electronics"
+            isActive={filter === "electronics"}
+            onClick={() => handleFilter("electronics")}
+          />
+          <Button
+            label="Jewelery"
+            isActive={filter === "jewelery"}
+            onClick={() => handleFilter("jewelery")}
+          />
+          <Button
+            label=" Men Clothing"
+            isActive={filter === "men's clothing"}
+            onClick={() => handleFilter("men's clothing")}
+          />
+          <Button
+            label="Women Clothing"
+            isActive={filter === "women's clothing"}
+            onClick={() => handleFilter("women's clothing")}
+          />
         </div>
       </section>
 
       {/* Product section */}
-      <section className="grid grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-        {data.map((product, index) => {
-          return (
-            <ProductCard
-              key={index}
-              title={product.title}
-              description={product.description}
-              price={product.price}
-              category={product.category}
-              image={product.image}
-            />
-          );
-        })}
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+        {(filter === "All" || filter === ""
+          ? data
+          : data.filter((p) => p.category === filter)
+        ).map((product, index) => (
+          <ProductCard
+            key={index}
+            title={product.title}
+            price={product.price}
+            image={product.image}
+            description={product.description}
+            category={product.category}
+          />
+        ))}
       </section>
     </div>
   );
