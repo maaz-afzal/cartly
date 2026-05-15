6a56d65dee483e7f74186171af5af8cc53f866ed
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-15 15:48:34 +0500

    add pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Favorites from "./pages/Favorites";
+import ProductDetail from "./pages/ProductDetail";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 };
