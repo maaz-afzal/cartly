5d752b9493e29449b72eff71b49764ead5240b14
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-05-15 15:22:18 +0500

    add Navbar component

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Heart, Search, ShoppingBag, Sun } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <header className="w-full bg-white">
+      <div className="mx-auto max-w-6xl px-4">
+        <div className="flex justify-between items-center py-5 w-full">
+          <div>
+            <span className="text-2xl font-bold font-serif tracking-widest">
+              CARTLY
+            </span>
+          </div>
+
+          <div className="flex items-center gap-5">
+            <div className="relative w-90">
+              <Search
+                className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+                size={18}
+              />
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-full rounded-full border border-gray-300 bg-white py-2 pl-10 pr-4 text-sm outline-none transition-all duration-200 focus:border-[#F0803C] focus:ring-2 focus:ring-[#F0803C]/30 placeholder:text-gray-400"
+              />
+            </div>
+
+            <div className="p-2 hover:bg-[#EEE5E9] rounded-full cursor-pointer transition duration-250">
+              <Heart size={20} />
+            </div>
+            <div className="relative p-2 hover:bg-[#EEE5E9] rounded-full cursor-pointer transition duration-200">
+              <ShoppingBag size={20} className="cursor-pointer" />
+
+              <span className="absolute -top-3 -right-2 bg-red-500 text-white rounded-full px-1.5 text-[10px] leading-none min-w-4 h-4 flex items-center justify-center">
+                0
+              </span>
+            </div>
+            <div className="p-2 hover:bg-[#EEE5E9] rounded-full cursor-pointer transition duration-250">
+              <Sun size={20} className="cursor-pointer" />
+            </div>
+
+            <button className="bg-[#256EFF] text-white px-6 py-2 rounded-full font-semibold active:scale-96 transition duration-300 cursor-pointer hover:opacity-90">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
